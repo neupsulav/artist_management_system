7ec37175401cab359942f01a8b26b45066c454c2
Sulav Neupane <neupsulav@gmail.com>
2026-03-18 19:51:19 +0545

    Base setup for server

---
+++ README.md
@@ -0,0 +1,26 @@
+## Setup instructions
+
+### 1. Clone the repository
+
+```bash
+git clone <repo-url>
+cd artist_management_system
+```
+
+### 2. Install dependencies
+
+```bash
+npm install
+```
+
+### 3. Environment configuration
+
+Copy `.env.example` to `.env` and add necessary values
+
+### 4. Running the application
+
+Start the server:
+
+```bash
+npm start
+```
---
+++ server/server.js
@@ -0,0 +1,24 @@
+const http = require("http");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  // Common headers (CORS)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS",
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
